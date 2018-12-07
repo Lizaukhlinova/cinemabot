55401a85065319cbfe79e71818ce16d332f1bcdb
ukhlinova <ukhlinova@yandex-team.ru>
2018-12-07 23:50:05 +0300

    init

---
+++ bot/kinopoisk/parse_query.py
@@ -0,0 +1,50 @@
+import urllib
+import requests
+import re
+import html
+
+kinopoisk_url = 'https://www.kinopoisk.ru/'
+
+
+class Film:
+    def __init__(self, name, url, year):
+        self.name = name
+        self.url = url
+        self.year = year
+
+
+def parse_a_tag(tag):
+    name = re.search(b'data-type[\W\w]*?</a>', tag).group(0)[:-4]
+    print(name)
+    name = re.split(b'>', name)[1].decode(encoding='cp1251')
+    name = html.unescape(name)
+    url = re.search(b'film/[0-9]*', tag).group(0).decode(encoding='cp1251')
+    year = re.search(b'"year">[\W\w]*?<', tag)
+    if year:
+        year = year.group(0)[7:-1].decode(encoding='cp1251')
+        year = html.unescape(year)
+    return Film(name, url, year)
+
+
+def find_top_five_by_name(film):
+    film = urllib.parse.quote(film)
+    search_response = requests.get(kinopoisk_url + 'index.php?kp_query='
+                                   + film + '&what=')
+    if search_response.status_code != 200:
+        print('Something went wrong...')
+        return
+    found_tags = [match for match in re.findall(
+        b'<p class="name"><a href="/level/1/film/[0-9]*/sr/1/[\W\w]*?</p>',
+        search_response.content)][:5]
+    found_films = [parse_a_tag(tag) for tag in found_tags]
+    return found_films
+
+
+def list_of_films(films):
+    res = ''
+    if films:
+        for i in range(len(films)):
+            res += '{}. {}, {}\n'.format(i + 1, films[i].name, films[i].year or '-')
+    else:
+        res = "К сожалению, по твоему запросу ничего не найдено :(\n"
+    return res[:-1]
---
+++ bot/main.py
@@ -0,0 +1,58 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from kinopoisk.parse_query import find_top_five_by_name, list_of_films
+
+from aiogram.utils.helper import Helper, HelperMode, ListItem
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+bot = Bot(token='663290175:AAGT2h1895I-xZ_7Ma2VsqcyrQDa2ecV7p8')
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+CHOICES = {'1', '2', '3', '4', '5', 'Ничего не подходит'}
+
+
+class FilmStates(Helper):
+    mode = HelperMode.snake_case
+
+    FILM_STATE_0 = ListItem()
+    FILM_STATE_1 = ListItem()
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    state = dp.current_state(user=message.from_user.id)
+    await message.reply("Ура, я супер!")
+    await state.set_state(FilmStates.FILM_STATE_0)
+
+
+@dp.message_handler(state=FilmStates.FILM_STATE_1)
+async def choose_film(message: types.Message):
+    print(message.text)
+    if message.text not in CHOICES:
+        await message.reply("Попробуйте уточнить название для более качественного поиска.")
+    else:
+        await message.reply("Вы выбрали {}".format(message.text))
+    state = dp.current_state(user=message.from_user.id)
+    await state.set_state(FilmStates.FILM_STATE_0)
+
+
+@dp.message_handler(state='*')
+async def search_film(message: types.Message):
+    print(message.text)
+    state = dp.current_state(user=message.from_user.id)
+    film_name = message.text
+    films = find_top_five_by_name(film_name)
+    msg = list_of_films(films)
+    await state.set_state(FilmStates.all()[1])
+    await message.reply(msg)
+
+
+@dp.message_handler(commands=['search_film'])
+async def send_question(message: types.Message):
+    await message.reply("Какой фильм будем искать?")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
