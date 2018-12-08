7a0733e95a7a7af20f58d7a10be8871842feeebb
ukhlinova <ukhlinova@yandex-team.ru>
2018-12-08 20:08:10 +0300

    content -> text

--- bot/kinopoisk/parse_film_page.py
+++ bot/kinopoisk/parse_film_page.py
@@ -16,23 +16,27 @@ def get_description(pattern):
 
 def set_film_info(film):
     search_response = requests.get(film.url)
+    print(film.url)
     if search_response.status_code != 200:
         print('Something went wrong...')
         return
-    img_pattern = re.search(b'openImgPopup\(\'[\W\w]*?\'\)',
-                            search_response.content)
+    print(len(search_response.content))
+    img_pattern = re.search('openImgPopup\(\'[\W\w]*?\'\)', search_response.text)
+    print(img_pattern)
     img_url = None
     if img_pattern:
-        img_url = get_img_url(img_pattern.group(0).decode('cp1251'))
+        img_url = get_img_url(img_pattern.group(0))
+        print(img_url)
         if img_url == common.no_poster_url:
             img_url = None
     film.set_image(img_url)
 
-    description = re.search(b'<div class="brand_words '
-                            b'film-synopsys"[\W\w]*?<\/div>',
-                            search_response.content)
+    description = re.search('<div class="brand_words '
+                            'film-synopsys"[\W\w]*?<\/div>',
+                            search_response.text)
     if description:
-        description = description.group(0).decode('cp1251')
+        description = description.group(0)
         description = get_description(html.unescape(description))
+        print(description)
     film.set_description(description)
     return film
